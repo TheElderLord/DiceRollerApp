b0ba7c2228df697c45acab715b95a381dc8a5f90
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2022-07-07 02:55:52 +0600

    Uploading image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,30 +11,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button)
+//        val rollButton: Button = findViewById(R.id.button)
         val button2:Button =findViewById(R.id.button2)
-        button2.setOnClickListener({roll2()})
-        rollButton.setOnClickListener() {
-//            val diceRange =1..6
-//            diceText.text = "${diceRange.random()}"
-//            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-//            toast.show()
-            rollDice()
-        }
+        button2.setOnClickListener({rollDice()})
+        rollDice()
+
     }
+
 
     private fun rollDice() {
         val dice = Dice(20)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val img : ImageView = findViewById(R.id.imageView)
+        val imgsource = when(diceRoll){
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_3
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+
+            else -> R.drawable.dice_6
+        }
+       img.contentDescription = diceRoll.toString()
+        img.setImageResource(imgsource)
+//        val resultTextView: TextView = findViewById(R.id.textView)
+//        resultTextView.text = diceRoll.toString()
 
     }
-    private fun roll2(){
-        val rand = Dice(6)
-        val tview :TextView = findViewById(R.id.textView2)
-        tview.text = rand.roll().toString()
-    }
+
 }
 
 class Dice(val numSides: Int) {
